06abb4a61efeef07ac0b07473962df182b703a84
madurihansanie <madurihansanie@gmail.com>
2019-08-20 10:58:58 +0530

    render categories

--- app-frontend/src/index.js
+++ app-frontend/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//COMPONENT
+import Category from './components/category';
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+ReactDOM.render(<Category />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
